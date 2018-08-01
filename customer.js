ccc124b3a92990ead717cd22dd8544ab9332d816
Justin V <justinvuser@gmail.com>
2018-08-01 14:22:30 -0400

    Javascript File Updated

--- customer.js
+++ customer.js
@@ -1,1 +1,31 @@
 var mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "",
+  database: ""
+});
+
+connection.connect(function(err) {
+   if (err)
+   throw err;
+    console.log("Connection as " + connection.threadId);
+
+    afterConnection();
+  });
+  
+function afterConnection() {
+ connection.query("SELECT * FROM products", function(err, res) {
+    if (err)
+    throw err;
+    console.log(res);
+    
+  connection.end();
+   });
+  }
+  
